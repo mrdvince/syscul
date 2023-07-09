0a08df59dca084ddc8fc10cba2ba312ac9dfcbbc
Vince <vincivenv@gmail.com>
2023-07-09 19:29:02 +0200

    Exit QEMU after test run

--- src/main.rs
+++ src/main.rs
@@ -28,17 +28,34 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::{Port, PortGeneric};
+    unsafe {
+        let mut port: PortGeneric<u32, x86_64::instructions::port::ReadWriteAccess> =
+            Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success)
 }
 
 #[test_case]
 fn trivial_assertion() {
     print!("Trivial assertion.....");
-    assert_eq!(1,2);
+    assert_eq!(1, 1);
     println!("[ok]")
 }
