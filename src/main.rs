c70b6617078f774351da3c5cbc596193a20fbe22
Vince <vincivenv@gmail.com>
2023-07-09 18:02:01 +0200

    custom test framework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 mod vga_buffer;
 
 #[allow(unused_imports)]
@@ -7,9 +11,14 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Romeo: Yo, Mercutio, my man! Why the long face?\nMercutio: Romeo, \
+    println!(
+        "Romeo: Yo, Mercutio, my man! Why the long face?\nMercutio: Romeo, \
     my brother from another mother, I'm just feeling some bad vibes today, you know? \
-    Like the universe is messing with my flow.");
+    Like the universe is messing with my flow."
+    );
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
@@ -17,4 +26,19 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("Trivial assertion.....");
+    assert_eq!(1,2);
+    println!("[ok]")
 }
